0afe9c7569f278e4f3b0a83af302c6dfd76b3458
Egor Erofeev <egor.erofeev@gmail.com>
2015-05-24 20:17:19 +0200

    PersonDTO

--- dozer-tools/src/test/java/com/gepardec/javatools/dozer/DTOTransformationTest.java
+++ dozer-tools/src/test/java/com/gepardec/javatools/dozer/DTOTransformationTest.java
@@ -5,6 +5,8 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import com.gepardec.javatools.dozer.MappingTestHandler.MappingTest;
+import com.gepardec.javatools.dozer.objects.domain.Person;
+import com.gepardec.javatools.dozer.objects.dto.PersonDTOV3;
 import com.gepardec.javatools.dozer.objects.first.ExampleDTO;
 import com.gepardec.javatools.dozer.objects.second.ExampleDomainObject;
 
@@ -15,5 +17,9 @@ public class DTOTransformationTest {
 	@Test @MappingTest(from=ExampleDTO.class, to=ExampleDomainObject.class)
 	public void test(){
 		handler.ignoreField("header");
+	}
+	
+	@Test @MappingTest(from=PersonDTOV3.class, to=Person.class)
+	public void testPersonMapping(){
 	}
 }
---
+++ dozer-tools/src/test/java/com/gepardec/javatools/dozer/objects/dto/PersonDTOV1.java
@@ -0,0 +1,13 @@
+package com.gepardec.javatools.dozer.objects.dto;
+
+public class PersonDTOV1 {
+	private String vorname;
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+}
