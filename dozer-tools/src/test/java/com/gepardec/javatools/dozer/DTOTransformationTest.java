fa92bd9b784ca4ba4376d3658ed4e62375d092af
Erhard Siegl <erhard.siegl@objectbay.com>
2015-05-26 09:24:20 +0200

    Test for PersonDTOV2

--- dozer-tools/src/test/java/com/gepardec/javatools/dozer/DTOTransformationTest.java
+++ dozer-tools/src/test/java/com/gepardec/javatools/dozer/DTOTransformationTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import com.gepardec.javatools.dozer.MappingTestHandler.MappingTest;
 import com.gepardec.javatools.dozer.objects.domain.Person;
+import com.gepardec.javatools.dozer.objects.dto.PersonDTOV2;
 import com.gepardec.javatools.dozer.objects.dto.PersonDTOV3;
 import com.gepardec.javatools.dozer.objects.first.ExampleDTO;
 import com.gepardec.javatools.dozer.objects.second.ExampleDomainObject;
@@ -15,11 +16,16 @@ public class DTOTransformationTest {
 	public MappingTestHandler handler = new MappingTestHandler(new DozerBeanMapper());	
 	
 	@Test @MappingTest(from=ExampleDTO.class, to=ExampleDomainObject.class)
-	public void test(){
+	public void testIgnoreField(){
 		handler.ignoreField("header");
 	}
 	
 	@Test @MappingTest(from=PersonDTOV3.class, to=Person.class)
-	public void testPersonMapping(){
+	public void testPersonV3Mapping(){
 	}
+	
+	@Test @MappingTest(from=PersonDTOV2.class, to=Person.class)
+	public void testPersonV2Mapping(){
+	}
+
 }
